7d9bdac3465f2a7d5fce2173d962b416c8430a07
Zian Chen <87293750+ziancn@users.noreply.github.com>
2026-02-26 14:41:41 +0800

    Update protocol.py

--- src/emsx_api_py/modules/protocol.py
+++ src/emsx_api_py/modules/protocol.py
@@ -4,19 +4,13 @@ EMSX modules protocol template
 
 import blpapi
 
-from typing import Protocol, TYPE_CHECKING
-
-
-if TYPE_CHECKING:
-    from ..session_manager import SessionManager
+from typing import Protocol
 
 
 class ModuleProtocol(Protocol):
-    def handle_event(
+    def process_event(
             self,
-            etype  : int,
             event  : blpapi.Event,
             session: blpapi.Session,
-            manager: "SessionManager",
     ):
         ...
