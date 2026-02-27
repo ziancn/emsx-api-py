41bd185fd97133d0e09390accad0bf665384ee45
ziancn <ziancn@hotmail.com>
2026-02-27 18:06:19 +0800

    Formatting
    # Conflicts:
    #	src/emsx_api_py/modules/status_monitor.py

--- src/emsx_api_py/modules/status_monitor.py
+++ src/emsx_api_py/modules/status_monitor.py
@@ -1,6 +1,5 @@
 """
-This module monitors general status like
-session status, services status, subscription status.
+This module monitors general status like session status, services status, subscription status, etc.
 """
 
 import blpapi
@@ -9,46 +8,72 @@ import logging
 from .protocol import ModuleProtocol
 
 
+# blpapi names
+SLOW_CONSUMER_WARNING         = blpapi.Name("SlowConsumerWarning")
+SLOW_CONSUMER_WARNING_CLEARED = blpapi.Name("SlowConsumerWarningCleared")
+
+SESSION_STARTED               = blpapi.Name("SessionStarted")
+SESSION_TERMINATED            = blpapi.Name("SessionTerminated")
+SESSION_STARTUP_FAILURE       = blpapi.Name("SessionStartupFailure")
+SESSION_CONNECTION_UP         = blpapi.Name("SessionConnectionUp")
+SESSION_CONNECTION_DOWN       = blpapi.Name("SessionConnectionDown")
+
+SERVICE_OPENED                = blpapi.Name("ServiceOpened")
+SERVICE_OPEN_FAILURE          = blpapi.Name("ServiceOpenFailure")
+
+SUBSCRIPTION_FAILURE          = blpapi.Name("SubscriptionFailure")
+SUBSCRIPTION_STARTED          = blpapi.Name("SubscriptionStarted")
+SUBSCRIPTION_TERMINATED       = blpapi.Name("SubscriptionTerminated")
+
+
 class StatusMonitor(ModuleProtocol):
+    """
+    Logging module
+    """
+    def __init__(self):
+        self._log_prefix = f"[{self.__class__.__name__}]"
+
+
     def process_event(
             self,
             event  : blpapi.Event,
             session: blpapi.Session,
     ):
         et = event.eventType()
-        if   et == blpapi.Event.SESSION_STATUS     : self.process_session_status_event(event, session)
-        elif et == blpapi.Event.SERVICE_STATUS     : self.process_service_status_event(event, session)
-        elif et == blpapi.Event.SUBSCRIPTION_STATUS: self.process_subscription_status_event(event, session)
+        if   et == blpapi.Event.SESSION_STATUS      : self.process_session_status_event(event, session)
+        elif et == blpapi.Event.SERVICE_STATUS      : self.process_service_status_event(event, session)
+        elif et == blpapi.Event.SUBSCRIPTION_STATUS : self.process_subscription_status_event(event, session)
+        elif et == blpapi.Event.ADMIN               : self.process_admin_event(event, session)
         else: pass
 
 
     def process_session_status_event(self, event: blpapi.Event, session: blpapi.Session):
         for msg in event:
-            if msg.messageType() == blpapi.Name("SessionStarted"):
-                logging.info(f"[{self.__class__.__name__}] Session started...")
-            elif msg.messageType() == blpapi.Name("SessionStartupFailure"):
-                logging.error(f"[{self.__class__.__name__}] Session startup failed")
-            else:
-                logging.info(f"[{self.__class__.__name__}] {msg}")
+            if   msg.messageType() == SESSION_STARTED         : logging.info(f"{self._log_prefix} Session started...")
+            elif msg.messageType() == SESSION_STARTUP_FAILURE : logging.error(f"{self._log_prefix} Session startup failed")
+            elif msg.messageType() == SESSION_CONNECTION_UP   : logging.info(f"{self._log_prefix} Session connection is up")
+            elif msg.messageType() == SESSION_CONNECTION_DOWN : logging.info(f"{self._log_prefix} Session connection is down")
+            elif msg.messageType() == SESSION_TERMINATED      : logging.info(f"{self._log_prefix} Session terminated")
+            else: logging.info(f"{self._log_prefix} {msg}")
 
 
     def process_service_status_event(self, event: blpapi.Event, session: blpapi.Session):
         for msg in event:
-            if msg.messageType() == blpapi.Name("ServiceOpened"):
-                logging.info(f"[{self.__class__.__name__}] Service opened...")
-            elif msg.messageType() == blpapi.Name("ServiceOpenFailure"):
-                logging.error(f"[{self.__class__.__name__}] Service failed to open")
-            else:
-                logging.info(f"[{self.__class__.__name__}] {msg}")
+            if   msg.messageType() == SERVICE_OPENED       : logging.info(f"{self._log_prefix} Service opened...")
+            elif msg.messageType() == SERVICE_OPEN_FAILURE : logging.error(f"{self._log_prefix} Service failed to open")
+            else: logging.info(f"{self._log_prefix} {msg}")
 
 
     def process_subscription_status_event(self, event: blpapi.Event, session: blpapi.Session):
         for msg in event:
-            if msg.messageType() == blpapi.Name("SubscriptionStarted"):
-                logging.info(f"[{self.__class__.__name__}] Subscription started...")
-            elif msg.messageType() == blpapi.Name("SubscriptionFailure"):
-                logging.error(f"[{self.__class__.__name__}] Subscription failed to start")
-            elif msg.messageType() == blpapi.Name("SubscriptionTerminated"):
-                logging.error(f"[{self.__class__.__name__}] Subscription terminated")
-            else:
-                logging.info(f"[{self.__class__.__name__}] {msg}")
+            if   msg.messageType() == SUBSCRIPTION_STARTED    : logging.info(f"{self._log_prefix} Subscription started...")
+            elif msg.messageType() == SUBSCRIPTION_FAILURE    : logging.error(f"{self._log_prefix} Subscription failed to start： {msg}")
+            elif msg.messageType() == SUBSCRIPTION_TERMINATED : logging.error(f"{self._log_prefix} Subscription terminated")
+            else: logging.info(f"{self._log_prefix} {msg}")
+
+
+    def process_admin_event(self, event: blpapi.Event, session: blpapi.Session):
+        for msg in event:
+            if   msg.messageType() == SLOW_CONSUMER_WARNING         : logging.warning(f"{self._log_prefix} SLOW CONSUMER WARNING")
+            elif msg.messageType() == SLOW_CONSUMER_WARNING_CLEARED : logging.info(f"{self._log_prefix} SLOW CONSUMER WARNING cleared")
+            else: logging.info(f"{self._log_prefix} {msg}")
