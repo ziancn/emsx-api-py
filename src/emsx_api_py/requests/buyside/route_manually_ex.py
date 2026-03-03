02abe3cd9bbd5d2863cda7ebd025c95fe32d9cc2
ziancn <ziancn@hotmail.com>
2026-03-03 21:53:02 +0800

    Placeholder

---
+++ src/emsx_api_py/requests/buyside/route_manually_ex.py
@@ -0,0 +1,7 @@
+"""
+The RouteManuallyEx request is generally used for phone orders where the placement is external to EMSX API.
+This request creates an order and notifies EMSX<GO> that this order is routed to the execution venue.
+
+We seldom use this request. Will leave this wrapper empty first.
+2026-03-03
+"""
